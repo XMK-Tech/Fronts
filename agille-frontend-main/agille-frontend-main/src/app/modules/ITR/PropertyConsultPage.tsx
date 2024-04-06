da0e9915b0fe86de1d7c7a7dc05baff768807350
David Mota <83174541+davidsilvamota@users.noreply.github.com>
2024-04-06 11:16:22 -0300

    create component dashboarPie

--- agille-frontend-main/agille-frontend-main/src/app/modules/ITR/PropertyConsultPage.tsx
+++ agille-frontend-main/agille-frontend-main/src/app/modules/ITR/PropertyConsultPage.tsx
@@ -14,8 +14,8 @@ import ModeloModal, {ModalErrorFooter} from '../../utils/components/ModeloModal'
 import {CustomButton} from '../../components/CustomButton/CustomButton'
 import {RegisterFormModelInput} from '../../../components/RegisterFormModel'
 import {masks} from '../../components/Form/FormInput'
-import {CustomDropdown} from './CustomDropdown'
 import {getExportProperty} from '../../services/FiscalProcedureApi'
+import * as V from 'victory'
 
 const PropietyListTable: React.FC<TablePropsRows> = (props) => {
   return (
@@ -138,6 +138,7 @@ export default function PropertyConsultPage(props: PropertyConsultPageProps) {
     <>
       <div className='card'>
         <div className='card-header border-0 pt-5'>
+          <DashboardPieComponente />
           <h3 className='card-title align-items-start flex-column'>
             <span className='card-label fw-bolder fs-3 mb-1'>Imóveis</span>
           </h3>
@@ -196,56 +197,46 @@ export default function PropertyConsultPage(props: PropertyConsultPageProps) {
             </div>
           }
           body={
-            <div className='d-flex flex-column'>
-              <div>Digite seu nome e cpf para gerar o laudo da propriedade</div>
-              <div className='card-body d-flex flex-column justify-content-around my-8  '>
-                <RegisterFormModelInput
-                  label={'Nome'}
-                  placeholder='Digite o nome'
-                  type='text'
-                  touched={undefined}
-                  errors={undefined}
-                  fieldProps={{
-                    value: nameReport,
-                    name: 'name',
-                    onBlur: () => {},
-                    onChange: (input: any) => {
-                      setNameReport(input.target.value)
-                    },
-                  }}
-                />
-                <RegisterFormModelInput
-                  label={'CPF'}
-                  placeholder='Digite o CPF'
-                  type='text'
-                  touched={undefined}
-                  errors={undefined}
-                  mask={masks.cpf}
-                  fieldProps={{
-                    value: cpfReport,
-                    name: 'document',
-                    onBlur: () => {},
-                    onChange: (input: any) => {
-                      setCpfReport(input.target.value)
-                    },
-                  }}
-                />
-                {/* <RegisterFormModelInput
-                            label={'CIB'}
-                            placeholder='Digite o CIB'
-                            type='text'
-                            touched={undefined}
-                            errors={undefined}
-                            mask={masks.cib}
-                            fieldProps={{
-                              value: 'cib',
-                              name: 'cib',
-                              onBlur: () => {},
-                              onChange: (input: any) => {
-                                // setCib(input.target.value)
-                              },
-                            }}
-                          /> */}
+            <div className='d-flex justify-content-around'>
+              <div className='d-flex flex-column'>
+                <div>Digite seu nome e cpf para gerar o laudo da propriedade</div>
+                <div className='card-body d-flex flex-column justify-content-around my-8  '>
+                  <RegisterFormModelInput
+                    label={'Nome'}
+                    placeholder='Digite o nome'
+                    type='text'
+                    touched={undefined}
+                    errors={undefined}
+                    fieldProps={{
+                      value: nameReport,
+                      name: 'name',
+                      onBlur: () => {},
+                      onChange: (input: any) => {
+                        setNameReport(input.target.value)
+                      },
+                    }}
+                  />
+                  <RegisterFormModelInput
+                    label={'CPF'}
+                    placeholder='Digite o CPF'
+                    type='text'
+                    touched={undefined}
+                    errors={undefined}
+                    mask={masks.cpf}
+                    fieldProps={{
+                      value: cpfReport,
+                      name: 'document',
+                      onBlur: () => {},
+                      onChange: (input: any) => {
+                        setCpfReport(input.target.value)
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+              <div className='d-flex flex-column'>
+                <div>Dados do laudo</div>
+                <DashboardPieComponente />
               </div>
             </div>
           }
@@ -267,5 +258,48 @@ export default function PropertyConsultPage(props: PropertyConsultPageProps) {
         ></ModeloModal>
       </div>
     </>
+  )
+}
+export function DashboardPieComponente() {
+  const data = [
+    {x: 'Lavoura Aptidão Boa', y: 10},
+    {x: 'Lavoura Aptidão Regular', y: 20},
+    {x: 'Lavoura Aptidão Restrita', y: 30},
+    {x: 'Pastagem Plantada', y: 20},
+    {x: 'Silvicultura ou Pastagem Natural', y: 10},
+    {x: 'Preservação da Fauna ou Flora', y: 10},
+  ]
+  const itemColor = ['#0FC2C0', '#4C5958', '#008F8C', '#015958', '#023535', '#8AA6A3']
+  return (
+    <div className='d-flex align-items-center'>
+      <div style={{width: 300, height: 300}}>
+        <svg viewBox='0 0 400 400'>
+          <V.VictoryPie
+            colorScale={['#0FC2C0', '#4C5958', '#008F8C', '#015958', '#023535', '#8AA6A3']}
+            standalone={false}
+            width={400}
+            height={400}
+            data={data}
+            innerRadius={68}
+            labelRadius={100}
+            labels={data.map((e)=>e.y)}
+            style={{labels: {fontSize: 20, fill: 'white'}}}
+          />
+          <V.VictoryLabel textAnchor='middle' style={{fontSize: 20}} x={200} y={200} text='' />
+        </svg>
+      </div>
+      <div>
+        <div>
+          {data.map((e, i) => (
+            <div className='d-flex align-items-center mb-4'>
+              <div style={{width: 8, height: 8, backgroundColor: itemColor[i],marginRight:8, borderRadius:'50%'}}></div>
+              <div>
+                {e.x} %{e.y}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
